6a7b68708b38cf4d38cf501f86b6c0b70a6f7b48
Rafal Zygula <p0hmgxcn@students.codeinstitute.net>
2024-07-15 15:37:17 +0000

    Declare main variables and map locations array in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("Project2");
+// Declare map locations data array
+const locations = [{
+        name: "Holy Cross Mountains",
+        description: "'They may not be the tallest, but they are certainly some of the oldest natural formations in Europe. (...) Lands where pagans loved to live freely. Where the long lustful nights full of barbaric drinks and rituals around the bonfire gave birth to the legends of Baba Yaga.'",
+        col: 64,
+        row: 24
+    },
+    {
+        name: "High School",
+        description: "'The first days of year one were the most important. Older students would recognize who was made of what after primary school. Traditional bullying greetings had begun. Bullies searched for the cats on first breaks. A cat was a rookie. A new guy. Someone they didn’t know. Someone they could cut down in size.'",
+        col: 15,
+        row: 69
+    },
+    // Add 4-6 locations more
+]
+
+// Setting up main variables for the Map Section
+const mapContainer = document.getElementById('map-container');
+const mapImage = document.getElementById('map-image');
+const mapDescription = document.getElementById('map-description');
