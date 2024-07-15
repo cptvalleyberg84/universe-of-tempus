990e35a3196c486da11516eae42ae04532a40f15
Rafal Zygula <p0hmgxcn@students.codeinstitute.net>
2024-07-15 17:43:47 +0000

    Fix floating dot position on the map

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,14 @@
 const locations = [{
         name: "Holy Cross Mountains",
         description: "'They may not be the tallest, but they are certainly some of the oldest natural formations in Europe. (...) Lands where pagans loved to live freely. Where the long lustful nights full of barbaric drinks and rituals around the bonfire gave birth to the legends of Baba Yaga.'",
-        col: 25,
-        row: 25
+        col: 64,
+        row: 27
     },
     {
         name: "High School",
         description: "'The first days of year one were the most important. Older students would recognize who was made of what after primary school. Traditional bullying greetings had begun. Bullies searched for the cats on first breaks. A cat was a rookie. A new guy. Someone they didn’t know. Someone they could cut down in size.'",
-        col: 35,
-        row: 35
+        col: 15,
+        row: 72
     },
     // Add 4-6 locations more
 ]
